bc679b18b0b6252fbf6121113b3ae2961d08e316
Iordancho <127532874+Iordancho@users.noreply.github.com>
2023-03-24 03:06:14 +0200

    Add files via upload

---
+++ Inheritance-Lab/04. Random List/RandomList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString(List<string> list)
+        {
+            Random random = new Random();
+            return list[random.Next(0, Count)];
+        }
+    }
+}
