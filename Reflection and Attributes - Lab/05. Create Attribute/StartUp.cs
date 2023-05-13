21536ee4f0f368b0f2d6043e6b86b57579710d06
Iordancho <127532874+Iordancho@users.noreply.github.com>
2023-05-14 02:16:53 +0300

    Add files via upload

---
+++ Reflection and Attributes - Lab/05. Create Attribute/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
