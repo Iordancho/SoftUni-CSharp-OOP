85c08197c5c825c075d849a024b3d63058d4be01
Iordancho <127532874+Iordancho@users.noreply.github.com>
2023-05-14 02:07:51 +0300

    Add files via upload

---
+++ Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new Exception("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+    }
+}
